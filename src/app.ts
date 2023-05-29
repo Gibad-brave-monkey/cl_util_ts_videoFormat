15451e9842b133cc71980fbdc997ebac7924e1a1
Ilyas Gibadullin <ilyas.gibadullin.199819987@gmail.com>
2023-05-29 20:08:42 +0300

    feat: 🎸 Add Prompt, Handlers

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
+import { PromptService } from "./core/prompt/prompt.service";
+
 export class App {
-  run() {
-    console.log("Run");
+  async run() {
+    const result = await new PromptService().input<number>("Число", "number");
+    console.log(result);
   }
 }
 
 const app = new App();
-
 app.run();
